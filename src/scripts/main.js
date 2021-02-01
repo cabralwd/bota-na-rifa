0d2cc4167ad5042b9fbf5d5f5859e8d26e2a3394
cbral <cbraljunior@icloud.com>
2021-02-01 17:08:44 -0300

    finality page index + styles init

---
+++ src/scripts/main.js
@@ -0,0 +1,36 @@
+class Accordion {
+  constructor(heading) {
+    this.heading = heading;
+  }
+
+  showOne() {
+    const accordionHeading = document.querySelectorAll(this.heading);
+
+    accordionHeading.forEach((item, key) => {
+      item.addEventListener('click', () => {
+        accordionHeading.forEach(element => {
+          element.classList.contains('active') ?
+            element.classList.remove('active') : null;
+        });
+
+        item.classList.add('active');
+      });
+    });
+  }
+
+  showAll() {
+    const accordionHeading = document.querySelectorAll(this.heading);
+
+    accordionHeading.forEach((item, key) => {
+      item.addEventListener('click', () => {
+        item.classList.contains('active') ?
+          item.classList.remove('active') :
+          item.classList.add('active');
+      });
+    });
+  }
+}
+
+const accordion = new Accordion('.tit');
+// for open every use showAll();
+accordion.showOne();
